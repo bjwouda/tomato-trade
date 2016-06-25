69f125227c5b68004aab20997ce7ceb3a9513eac
kai <kai.koehn@gmail.com>
2016-06-25 16:21:40 +0200

    .

--- app/helpers/capitalize.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-import _ from 'lodash/lodash';
-
-export function capitalize(params/*, hash*/) {
-  return _.capitalize(params);
-}
-
-export default Ember.Helper.helper(capitalize);
