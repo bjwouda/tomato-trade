2e91f89dbabfc2889b21d15860d6d8c63f127c53
kai <kai.koehn@gmail.com>
2016-12-14 21:07:56 +0100

    round clear fix

--- app/controllers/player.js
+++ app/controllers/player.js
@@ -4,11 +4,18 @@ import LangActions from '../mixins/lang-actions';
 
 import _ from 'lodash/lodash';
 
+let lastRound = 1
+
 export default Ember.Controller.extend(OfferActions, LangActions, {
 	game: Ember.computed.alias("model.userGame.content"),
 	_extOfferTomato: 0,
 
 	clearInputsOnNewRound: Ember.observer("game.roundCnt", function() {
+
+        let roundCnt = this.get('game.roundCnt')
+        if (roundCnt === lastRound) { return }
+        lastRound = roundCnt
+
 		this.set("_ext_offerTomato", "")
 		this.get("game.users").forEach((u) => {
 			u.set("_offerTomato", "")
