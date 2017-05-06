b910375e1b9987042ba4d7397a4b426fed68a57c
Berend Wouda <b.j.wouda@tudelft.nl>
2017-05-06 10:22:43 +0200

    Prettied up the tutorial navigation code with some components and icons.

--- app/controllers/player.js
+++ app/controllers/player.js
@@ -4,34 +4,35 @@ import LangActions from '../mixins/lang-actions';
 
 import _ from 'lodash/lodash';
 
-let lastRound = 1
+let lastRound = 1;
 
 export default Ember.Controller.extend(OfferActions, LangActions, {
-	game: Ember.computed.alias("model.userGame.content"),
-	_extOfferTomato: 0,
+  game: Ember.computed.alias("model.userGame.content"),
+  _extOfferTomato: 0,
   
   tutorialState: 0,
 
-	clearInputsOnNewRound: Ember.observer("game.roundCnt", function() {
+  clearInputsOnNewRound: Ember.observer("game.roundCnt", function() {
+    let roundCnt = this.get('game.roundCnt');
+    
+    if (roundCnt === lastRound) { return; }
+    
+    lastRound = roundCnt;
 
-        let roundCnt = this.get('game.roundCnt')
-        if (roundCnt === lastRound) { return }
-        lastRound = roundCnt
-
-		this.set("_ext_offerTomato", "")
-		this.get("game.users").forEach((u) => {
-			u.set("_offerTomato", "")
-			u.set("_offerPrice", "")
-		})
-	}),
+    this.set("_ext_offerTomato", "");
+    this.get("game.users").forEach((u) => {
+      u.set("_offerTomato", "");
+      u.set("_offerPrice", "");
+    });
+  }),
   
   actions: {
-    advanceTutorialState() {
-      this.set("tutorialState", this.get('tutorialState') + 1);
+    recedeTutorialState() {
+      this.set("tutorialState", this.get('tutorialState') - 1);
     },
     
-    resetTutorialState() {
-      this.set("tutorialState", 0);
+    advanceTutorialState() {
+      this.set("tutorialState", this.get('tutorialState') + 1);
     }
   }
 });
