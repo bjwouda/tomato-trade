f6c317db6e96adad303975c276ba074207218c3b
Berend Wouda <b.j.wouda@tudelft.nl>
2017-05-22 20:22:19 +0200

    Made chart utilities better. Working on individual player results.

--- app/components/player-results-player.js
+++ app/components/player-results-player.js
@@ -2,7 +2,37 @@ import Ember from 'ember';
 
 import ChartUtilities from "../mixins/chart-utilities";
 
+import _ from 'lodash/lodash';
+
 export default Ember.Component.extend(ChartUtilities, {
+  cooperative1Revenue: Ember.computed("histories.[]", "histories.@each", "round", function() {
+    let histories = this.get("histories");
+    let index = this.get("round");
+    
+    let offers = histories.filter(function(history) {
+      let sender = history.get("userSender");
+      let receiver = history.get("userReceiver");
+      let state = history.get("state");
+      let round = parseInt(history.get("round").split(/ /)[1]);
+      
+      let hasUsers = (sender.startsWith("seller 1") && (receiver.startsWith("buyer 1") || receiver.startsWith("buyer 2") || receiver.startsWith("External"))) || (receiver.startsWith("seller 1") && (sender.startsWith("buyer 1") || sender.startsWith("buyer 2") || sender.startsWith("External")));
+      let hasState = state === "accepted";
+      let hasRound = round === index;
+      
+      return hasUsers && hasState && hasRound;
+    });
+    
+    let revenue = offers.map(function(offer) {
+      let offerParameters = offer.get("offer").split(/, |:/);
+      
+      return offerParameters[1] * offerParameters[3];
+    }).reduce(function(revenue1, revenue2) {
+      return revenue1 + revenue2;
+    }, 0);
+    
+    return revenue;
+  }),
+  
   trader1RemainingTomatoes: Ember.computed("histories.[]", "histories.@each", function() {
     let histories = this.get('histories');
     
@@ -22,34 +52,133 @@ export default Ember.Component.extend(ChartUtilities, {
     return this.createChartData("results.player.remainingTomatoes", labels, data);
   }),
   
-  seller1Buyer2Offers: Ember.computed("histories.[]", "histories.@each", function() {
+  seller1Buyer2Offers: Ember.computed("histories.[]", "histories.@each", "round", function() {
     let histories = this.get("histories");
+    let index = this.get("round");
     
-    let offers = histories.filter(function(history) {
+    let offers1 = histories.filter(function(history) {
       let sender = history.get("userSender");
       let receiver = history.get("userReceiver");
       let state = history.get("state");
+      let round = parseInt(history.get("round").split(/ /)[1]);
       
-      let hasUsers = sender.startsWith("seller 1") && receiver.startsWith("buyer 2");
+      let hasUsers = (sender.startsWith("seller 1") && receiver.startsWith("buyer 1"));
       let hasState = state === "open" || state === "accepted" || state === "declined" || state === "confirmed" || state === "recalled - open" || state === "recalled - confirmed";
+      let hasRound = round === index;
       
-      return hasUsers && hasState;
+      return hasUsers && hasState && hasRound;
     });
     
-    let labels = offers.map(function(offer) {
-      return offer.get("state");
+    let offers2 = histories.filter(function(history) {
+      let sender = history.get("userSender");
+      let receiver = history.get("userReceiver");
+      let state = history.get("state");
+      let round = parseInt(history.get("round").split(/ /)[1]);
+      
+      let hasUsers = (receiver.startsWith("seller 1") && sender.startsWith("buyer 1"));
+      let hasState = state === "open" || state === "accepted" || state === "declined" || state === "confirmed" || state === "recalled - open" || state === "recalled - confirmed";
+      let hasRound = round === index;
+      
+      return hasUsers && hasState && hasRound;
     });
     
-    let data = offers.map(function(offer) {
+    let data1 = offers1.map(function(offer) {
       // Hacky way to get to the offer parameters in existing histories.
       let offerParameters = offer.get("offer").split(/, |:/);
       
       let tomatoes = offerParameters[1];
       let unitPrice = offerParameters[3];
       
-      return +(unitPrice);
+      let time = offer.get("ts");
+      
+      return {
+        x: moment(time),
+        y: +(unitPrice)
+      };
     });
     
-    return this.createChartData("results.player.seller1Buyer2Offers", labels, data);
+    let data2 = offers2.map(function(offer) {
+      // Hacky way to get to the offer parameters in existing histories.
+      let offerParameters = offer.get("offer").split(/, |:/);
+      
+      let tomatoes = offerParameters[1];
+      let unitPrice = offerParameters[3];
+      
+      let time = offer.get("ts");
+      
+      return {
+        x: moment(time),
+        y: +(unitPrice)
+      };
+    });
+    
+    let colors1 = offers1.map(function(offer) {
+      let state = offer.get("state");
+      
+		  let stateColors = {
+				"open"                 : "hsl(9,0%,100%)",
+				"confirmed"            : "hsl(69,100%,64%)",
+				"recalled - confirmed" : "hsl(9,0%,64%)",
+				"accepted"             : "hsl(129,100%,64%)",
+				"declined"             : "hsl(9,100%,64%)",
+				"recalled - open"      : "hsl(9,0%,64%)"
+		  };
+      
+      return stateColors[state];
+    });
+    
+    let colors2 = offers2.map(function(offer) {
+      let state = offer.get("state");
+      
+		  let stateColors = {
+				"open"                 : "hsl(9,0%,100%)",
+				"confirmed"            : "hsl(69,100%,64%)",
+				"recalled - confirmed" : "hsl(9,0%,64%)",
+				"accepted"             : "hsl(129,100%,64%)",
+				"declined"             : "hsl(9,100%,64%)",
+				"recalled - open"      : "hsl(9,0%,64%)"
+		  };
+      
+      return stateColors[state];
+    });
+    
+    let chartDataSet1 = this.createChartDataSet("results.player.seller1Buyer2Offers", data1, colors1);
+    let chartDataSet2 = this.createChartDataSet("results.player.seller1Buyer2Offers", data2, colors2);
+    
+    return this.createChartData([
+      chartDataSet1,
+      chartDataSet2
+    ]);
   }),
+  
+  options: {
+    scales: {
+      xAxes: [{
+        type: 'time',
+        position: 'bottom',
+        time: {
+          displayFormats: {
+            'second': "HH:mm:ss"
+          },
+          tooltipFormat: "DD-MM-YYYY HH:mm:ss",
+          unit: 'second',
+          unitStepSize: 30
+        }
+      }]
+    }
+  },
+  
+  round: 1,
+  
+  numberOfRounds: Ember.computed.alias("game.numberOfRounds"),
+  
+  rounds: Ember.computed("numberOfRounds", function() {
+    return _.range(1, 11);
+  }),
+  
+  actions: {
+    setRound(round) {
+      this.set("round", round);
+    }
+  }
 });
--- app/mixins/chart-utilities.js
+++ app/mixins/chart-utilities.js
@@ -3,25 +3,32 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
   i18n: Ember.inject.service(),
   
-  createChartData(key, labels, data) {
+  createChartData(dataSets, options, labels) {
     return {
       labels: labels,
-      datasets: [
-        {
-          label: this.get("i18n").t(key),
-          fill: false,
-          lineTension: 0,
-          backgroundColor: "hsl(9,100%,64%)",
-          borderColor: "hsl(9,100%,64%)",
-          pointBorderColor: "hsl(9,100%,44%)",
-          pointBackgroundColor: "hsl(9,100%,64%)",
-          borderWidth: 2,
-          pointRadius: 6,
-          pointHoverRadius: 8,
-          pointHitRadius: 16,
-          data: data
-        }
-      ]
+      datasets: dataSets,
+      options: options
+    }
+  },
+  
+  createChartDataSet(key, data, colors) {
+    if(typeof colors === "undefined") {
+      colors = "hsl(9,100%,64%)";
+    }
+    
+    return {
+      label: this.get("i18n").t(key),
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "hsl(9,100%,64%)",
+      borderColor: "hsl(9,100%,64%)",
+      pointBorderColor: "hsl(9,100%,44%)",
+      pointBackgroundColor: colors,
+      borderWidth: 2,
+      pointRadius: 6,
+      pointHoverRadius: 8,
+      pointHitRadius: 16,
+      data: data
     };
   }
 });
