2590d9358e4c25d33d6dc423c4d56e6b89e6162d
Berend Wouda <b.j.wouda@tudelft.nl>
2017-05-31 06:21:37 +0200

    Worked around a weird bug where sometimes shit don't work

--- app/components/player-offer-display.js
+++ app/components/player-offer-display.js
@@ -1,10 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  state: Ember.computed("offer.state", function() {
-    let state = this.get("offer.state");
+  offerState: Ember.computed("offer.state", function() {
+    let offerState = this.get("offer.state");
     
-    // Chop the tail end off recalled offers so we can properly localize their state.
-    return state.split(/[ -]+/)[0];
+    // Guards against a weird bug where sometimes offerState is not yet initialized.
+    if(offerState) {
+      // Chop the tail end off recalled offers so we can properly localize their state.
+      let offerStateParameters = offerState.split(/[ -]+/);
+      
+      return offerStateParameters[0];
+    }
+    else {
+      // This is nonsense but should be replaced soon as offerState is initialized.
+      return "open";
+    }
   })
 });
