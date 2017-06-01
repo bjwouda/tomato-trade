4e288d585e2808fe067a27190f4cf7d721039701
Berend Wouda <b.j.wouda@tudelft.nl>
2017-06-01 22:25:30 +0200

    Been working on the tutorial.

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -173,31 +173,58 @@ export default {
     "time": "Time",
     "weekly": "Weekly",
     "daily": "Daily",
-    "starting": "Starting...",
+    "starting": "Starting",
     "paused": "Paused",
     "gameOver": "Game over!",
     "timeUp": "Time up!"
   },
   "tutorial": {
     "player": {
-      "introduction": "Welcome to Tomato Trade! (With a picture?)",
-      "goal": "Doel SamenMarkt® Een betere toekomst voor de tuinbouw in Nederland Transparante prijsvorming Rationaliteit Stabiliteit Vertrouwen",
-      "tutorialExplanation": "This tutorial aims to let you know about tomatoes. It will do this by... In het kort Voorbereiding Spelverloop Resultaten",
+      "introduction1": "<h3 class='title-container'>Welcome to Tomato Trade!</h3><p class='text-center'><img src='assets/images/logos/inholland.png' class='image-logo' /><img src='assets/images/logos/samenmarkt.png' class='image-logo' /><img src='assets/images/logos/tudelft.png' class='image-logo' /></p><p class='text-center'>Use the arrows at the right side of these messages to move back and forth through the tutorial.</p>",
+      "introduction2": "This tutorial aims to make you familiar with the concepts of the <strong>tomato trade</strong>, and the <strong>functionality</strong> and <strong>flow</strong> of this game.",
+      
+      "trade1": "<strong>SamenMarkt®</strong> is an initiative of various growers, traders, and researchers in the Netherlands. Its goal is to provide a <strong>better future</strong> for the horticulture sector in the Netherlands, through <strong>transparant pricing</strong>, <strong>rationality</strong>, <strong>stability</strong>, and <strong>trust</strong>. This game was made and is played as part of this effort, allowing research into and training of digital forms of trading.",
+      "trade2": "<p>The tomato trade in the Netherlands roughly consists of a chain of four parties: <strong>growers</strong>, <strong>cooperatives</strong>, <strong>traders</strong>, and <strong>retailers</strong>. Tomatoes travel from the growers to the retailers, and money travels back from the retailers to the growers.</p><h3 class='title-container'>Growers<span class='glyphicon glyphicon-transfer tutorial-icon'></span>Cooperatives<span class='glyphicon glyphicon-transfer tutorial-icon'></span>Traders<span class='glyphicon glyphicon-transfer tutorial-icon'></span>Retailers</h3><p>This game focuses on the trade between <strong>cooperatives</strong> and <strong>traders</strong>. Cooperatives trade on behalf of the growers to negotiate a price that is as <strong>high</strong> as possible. Traders, on the other hand, trade on behalf of the retailers and wish to negotiate a price that is as <strong>low</strong> as possible in order to maximize their profits (the retail price is known in advance).</p><p>The other trades, between growers and cooperatives, and between traders and retailers, are known in advance and are represented in this game as the <strong>supply</strong> and the <strong>demand</strong>, respectively.</p>",
+      
+      "game1": "<p>The game is based around the trade between <strong>cooperatives</strong> and <strong>traders</strong>, and has you play as one of either role. There can be several players of each role.</p><p>Trading is done per week, and for each week the game will have you perform the <strong>weekly trade</strong> (the regular trade) and the <strong>daily trade</strong> (the last-minute trade) for the remaining tomatoes. Each of these types of trades is performed in a <strong>round</strong> of the game, which means that each week consists of two rounds (the weekly trade round and the daily trade round).</p>",
+      "game2": "<p>The <strong>goal</strong> of the game is to <strong>negotiate contracts</strong> for the delivery of tomatoes with the players of the opposite role, in order to <strong>perform better</strong> than the players with the same role as you by <strong>scoring a better average</strong> taken over the entire game.</p>",
+      "game3": "<p>Take note that both the <strong>supply</strong> and <strong>demand</strong> are only <strong>prognoses</strong> of the definitive amount. Only after the weekly trade, the amount you have to trade for will be made definitive by adjusting it with a random value. You will have to account for this possible change during both the weekly and the daily trade.</p><p>The prognosis can change by either <strong>0%</strong>, <strong>5%<strong>, or </strong>10%</strong>, in <strong>either direction</strong>. Each percentage is <strong>equally likely</strong> to occur, with <strong>0% twice as likely</strong to> to occur.</p>",
+      
+      "sellerIntroduction": "<p>You are playing a <strong>cooperative</strong>. Your job is to <strong>sell tomatoes</strong> to traders on behalf of the growers.</p><p>Your <strong>goal</strong> is achieve the <strong>highest possible unit price</strong> per sale, and the <strong>highest average unit price</strong> over the entire game.</p>",
+      "sellerPropertiesIntroduction": "These values represent your progress in a round. They will change as you perform successful trades.",
+      "sellerPropertySupply": "This is the supply of tomatoes (in kilograms) that you are expected to sell on behalf of the farmers this week.",
+      "sellerPropertySold": "This is the amount of tomatoes (in kilograms) that you have sold so far this week.",
+      "sellerPropertyRemainder": "This is the amount of tomatoes (in kilograms) that you still have to sell this week.",
+      "sellerPropertyAverageUnitPrice": "This is the average unit price (per kilogram) of all the trades that you have successfully performed this week.",
+      "sellerPropertiesConclusion": "Make sure to sell all of your supply. Any remaining tomatoes will spoil and have be dumped.",
+      "sellerConclusion": "",
+      
+      "buyerIntroduction": "<p>You are playing a <strong>trader</strong>. Your job is to <strong>buy tomatoes</strong> from cooperatives so you can sell them to the retailers (for the retail price that is known in advance). Note that you incur fixed costs every week, and that you will have to pay a fine if you do not meet the demand of the retailers.</p><p>Your <strong>goal</strong> is achieve the <strong>highest possible profit</strong>, and the <strong>highest average profit</strong> per unit of tomato over the entire game.</p>",
+      "buyerPropertiesIntroduction1": "These values represent your progress in a round. These values will change as you perform successful trades.",
+      "buyerPropertyDemand": "This is the demand of tomatoes (in kilograms) that you are expected to buy on behalf of the retailers this week.",
+      "buyerPropertyBought": "This is the amount of tomatoes (in kilograms) that you have bought so far this week.",
+      "buyerPropertyRemainder": "This is the amount of tomatoes (in kilograms) that you still have to buy this week.",
+      "buyerPropertyAverageUnitPrice": "This is the average unit price (per kilogram) of all the trades you have successfully performed this week.",
+      "buyerPropertiesConclusion1": "",
+      "buyerPropertiesIntroduction2": "These values represent your relationship with the retailers for this round.",
+      "buyerPropertyFine": "This is the fine (per kilogram) that you have to pay for the tomatoes you failed to buy for the retailers this week.",
+      "buyerPropertyFixedCosts": "These are the fixed costs you will incur this week.",
+      "buyerPropertyRetailPrice": "This is the price (per kilogram) that the retailers will pay you for their tomatoes this week.",
+      "buyerPropertiesConclusion2": "",
+      "buyerConclusion": "",
+      
+      "conclusion": "<p>This is the end of the tutorial. If you have any doubts you can rewind through the tutorial and take a second look at various parts of the tutorial.</p><h3 class='title-container'>Good luck!</h3>",
+      
+      
       "overview": "De tomatenhandel in Nederland verloopt ongeveer als volgt: [plaatje] Dit spel focust zich op de handel tussen coöperaties en handelshuizen Daarbij zijn het aanbod, de vraag en de retailprijs bekend Het spel speelt zich af in een aantal verschillende weken van het jaar Er zijn een aantal rollen te verdelen: coöperaties en handelshuizen Samen onderhandelen deze spelers over contracten voor de levering van tomaten Het doel is om beter te presteren dan je concurrenten",
       "sellerOverview": "Een coöperatie verkoopt tomaten namens telers aan handelshuizen Daarbij is het doel simpel: een zo hoog mogelijke uitbetaalprijs verkrijgen Elke coöperatie heeft Een aanbod aan tomaten (in kg)",
       "buyerOverview": "Een handelshuis koopt tomaten in om deze door te verkopen aan de retail Daarbij is het doel een zo hoog mogelijke winst te verkrijgen Elk handelshuis heeft Een vraag naar tomaten vanuit de retail (in kg) Een retailprijs die u krijgt voor uw tomaten (in €/kg) Vaste kosten (in €/kg) Een boete voor niet geleverde tomaten (in €/kg)",
       "flowWeek": "De levering van tomaten voor een bepaalde week bestaat uit twee delen: Het onderhandelen over weekcontracten voor de volgende week De dagcontracten voor de resterende tomaten Nu kunnen de onderhandelingen beginnen! Verkoop uw aanbod (coöperatie) of voldoe aan uw vraag (handelshuis) De spelers kunnen een bod naar elkaar sturen Na wederzijds accepteren is een contract gesloten Tijd per ronde = 6 of 4 minuten LET OP: uw aanbod of vraag is slechts een prognose, dit kan afwijken!",
-      "timer": "Dit vak laat zien: Uw rol en spelersnaam De week en het type contracten waarover onderhandeld wordt De resterende tijd",
-      "sellerProperties": "Dit vak laat zien: Uw aanbod (kg) Uw aantal verkochte tomaten (kg) Uw resterende aanbod (kg) Uw gemiddelde onderhandelprijs (€/kg) van deze ronde",
-      "buyerProperties": "Dit vak laat zien: Uw vraag (kg) Uw aantal gekochte tomaten (kg) Uw resterende vraag (kg) Uw gemiddelde tomatenprijs (€/kg) van deze ronde Uw boete voor het niet leveren van tomaten (€/kg vraag resterend) Uw vaste kosten (€/kg vraag) Uw verkoopprijs richting de retail (€/kg) Uw KPI: uw winst of verlies (€/kg vraag) over het gehele spel",
+      "clock": "Dit vak laat zien: Uw rol en spelersnaam De week en het type contracten waarover onderhandeld wordt De resterende tijd",
       "externalOffer": "Dit bod kan naast de andere biedingen worden geaccepteerd De geboden hoeveelheid is een maximum, u kunt akkoord gaan met minder LET OP: Dit bod blijft niet eeuwig geldig...",
       "flowDay": "Na de ronde voor de weekcontracten, volgen de dagcontracten! Hiermee worden de resterende tomaten verkocht of ingekocht Uw aanbod of vraag wordt nu pas definitief LET OP: Tomaten die resteren na de dagcontracten worden gedumpt (€0,00/kg) Niet voldaan aan de vraag? Dan krijgt u een bepaalde boete per kg",
-      "prognosis": "Voor de start met de daghandel wordt uw definitieve aanbod of vraag bepaald -10% -5% 0% 0% +5% +10% LET OP: Houd bij de weekhandel rekening met deze mogelijke afwijking",
       "ending": "[Resultaten...] De winnaars worden bepaald met de KPI's: De coöperatie met de hoogste uitbetaalprijs (gemiddeld) Het handelshuis met de hoogste winst (per kg vraag)",
       "watchFor": "Overgebleven tomaten leveren niks op Maar, sluit geen contracten voor tomaten die u niet heeft! Uw vraag of aanbod kan maximaal 10% afwijken Er is een boete bij onvoldoende tomaten voor de retail Na enkele minuten ontvangt u een extern bod",
-      "outroduction": "Succes!",
-      
-      "sellerPropertySupply": "Dit vak laat zien: Uw aanbod (kg) Uw aantal verkochte tomaten (kg) Uw resterende aanbod (kg) Uw gemiddelde onderhandelprijs (€/kg) van deze ronde",
     }
   },
   "table": {
