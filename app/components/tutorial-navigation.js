b3b9d4edb625c0c1e13ff496f88f4fc6a47b9bdb
Berend Wouda <b.j.wouda@tudelft.nl>
2017-06-19 20:43:43 +0200

    Hacked in keyboard support for tutorial skipping. Requires ember-keyboard addon, so don't forget npm install.

--- app/components/tutorial-navigation.js
+++ app/components/tutorial-navigation.js
@@ -2,6 +2,45 @@ import Ember from 'ember';
 
 import TutorialActions from '../mixins/tutorial-actions';
 
-export default Ember.Component.extend(TutorialActions, {
-  tagName: '',
+import { tutorialState } from '../helpers/tutorial-state';
+
+import { EKMixin, EKOnInsertMixin } from 'ember-keyboard';
+import { keyPress } from 'ember-keyboard';
+
+export default Ember.Component.extend(TutorialActions, EKMixin, EKOnInsertMixin, {
+  tagName: "",
+  
+  leftArrowKeyPress: Ember.on(keyPress("ArrowLeft"), function() {
+    // Duplicates controller logic, but what are you gonna do...
+    let tutorial = this.get("tutorial");
+    let previous = this.get("previous");
+    
+    if(previous !== null) {
+      if(typeof previous !== 'undefined') {
+        let state = tutorialState([tutorial, previous]);
+        
+        this.send("changeTutorialState", tutorial, state);
+      }
+      else {
+        this.send("recedeTutorialState", tutorial);
+      }
+    }
+  }),
+  
+  rightArrowKeyPress: Ember.on(keyPress("ArrowRight"), function() {
+    // Duplicates controller logic, but what are you gonna do...
+    let tutorial = this.get("tutorial");
+    let next = this.get("next");
+    
+    if(next !== null) {
+      if(typeof next !== 'undefined') {
+        let state = tutorialState([tutorial, next]);
+        
+        this.send("changeTutorialState", tutorial, state);
+      }
+      else {
+        this.send("advanceTutorialState", tutorial);
+      }
+    }
+  })
 });
